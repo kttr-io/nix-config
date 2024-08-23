224904f0f7722af4cb879ae6b7a9e9387ea91f0a
Michael Kötter <michael@m-koetter.de>
2024-08-23 21:26:51 +0000

    axion config updates

--- hosts/axion/default.nix
+++ hosts/axion/default.nix
@@ -9,9 +9,22 @@
     inputs.nixos-hardware.nixosModules.dell-xps-13-9370
     ./hardware.nix
 
+    inputs.disko.nixosModules.disko
+    ../../disk/luks-btrfs
+
     ../common/linux
     ../common/users/michael
   ];
+
+  disko.devices.disk.main.device = "/dev/nvme0n1";
+
+  zramSwap.enable = lib.mkDefault true;
+
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
 
   system.stateVersion = "24.05";
 }
