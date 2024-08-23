68a16a678c29f8f6e4059173014c733ce017ff4b
Michael Kötter <michael@m-koetter.de>
2024-08-23 23:50:17 +0200

    boot.initrd.systemd config

--- hosts/common/linux/bootloader.nix
+++ hosts/common/linux/bootloader.nix
@@ -4,7 +4,15 @@
 , pkgs
 , ...
 }: {
-  boot.loader.grub.enable = lib.mkDefault true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub = {
+    enable = lib.mkDefault true;
+    boot.loader.grub.efiSupport = true;
+    boot.loader.grub.efiInstallAsRemovable = true;
+  };
+
+  boot.initrd.systemd = {
+    enable = true;
+    enableTpm2 = true;
+  };
+
 }
