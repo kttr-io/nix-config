5c6eb8a0fe224e932f9ce08005586cc90f52a7cf
Michael Kötter <michael@m-koetter.de>
2024-08-23 13:15:51 +0200

    fix disk config

--- disk/btrfs/default.nix
+++ disk/btrfs/default.nix
@@ -1,34 +1,57 @@
 {
-  imports = [
-    ../hybrid
-  ];
 
-  disko.devices.disk.main.content.partitions.root.content = {
-    type = "btrfs";
-    extraArgs = [ "-f" ]; # Override existing partition
-    # Subvolumes must set a mountpoint in order to be mounted,
-    # unless their parent is mounted
-    subvolumes = {
-      "@root" = {
-        mountOptions = [ "compress=zstd" ];
-        mountpoint = "/";
-      };
-      "@home" = {
-        mountOptions = [ "compress=zstd" ];
-        mountpoint = "/home";
-      };
-      "@nix" = {
-        mountOptions = [ "compress=zstd" "noatime" ];
-        mountpoint = "/nix";
-      };
-      "@log" = {
-        mountOptions = [ "compress=zstd" "noatime" ];
-        mountpoint = "/var/log";
-      };
-      "@swap" = {
-        mountpoint = "/.swapvol";
-        swap = {
-          swapfile.size = "8G";
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "@root" = {
+                    mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/";
+                  };
+                  "@home" = {
+                    mountOptions = [ "compress=zstd" ];
+                    mountpoint = "/home";
+                  };
+                  "@nix" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/nix";
+                  };
+                  "@log" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/var/log";
+                  };
+                  "@swap" = {
+                    mountpoint = "/.swapvol";
+                    swap = {
+                      swapfile.size = "8G";
+                    };
+                  };
+                };
+              };
+            };
+          };
         };
       };
     };
