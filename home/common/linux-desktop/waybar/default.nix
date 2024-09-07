ae6affbecb88b0ebf5fd29c7455b12cd8d3c0d33
Michael Kötter <michael@m-koetter.de>
2024-09-07 23:07:53 +0200

    hide bluetooth when there is no controller

--- home/common/linux-desktop/waybar/default.nix
+++ home/common/linux-desktop/waybar/default.nix
@@ -95,6 +95,7 @@ in
 
           "bluetooth" = {
             format = "";
+            format-no-controller = "";
             tooltip-format = "{controller_alias}\t{controller_address}";
             tooltip-format-connected = "{controller_alias}\t{controller_address}\n\n{device_enumerate}";
             tooltip-format-enumerate-connected = "{device_alias}\t{device_address}";
